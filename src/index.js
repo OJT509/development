18792a7f3ba62bbe4b6f914d69bcc62d74155187
Oliver <oliverthorpe509@hotmail.co.uk>
2022-03-29 19:37:57 +0100

    Add test scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
 const componentService = new ComponentService();
+const version = require("../package.json").version;
 run(alertService, componentService);
 
-console.log("TEST!");
+document.getElementById("version").innerHTML = "Version " + version;
